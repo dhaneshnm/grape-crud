4b9570a61c6c050287f266c30548ec6c4b29487b
dragon364 <dhanesh.n.m19@gmail.com>
2015-09-09 17:29:14 -0400

    some failing tests

--- test/api_test.rb
+++ test/api_test.rb
@@ -15,4 +15,18 @@ class APITest < MiniTest::Test
 			assert !content.nil?
 		end
         end
+
+	def test_id_path
+	   @paths  = YAML.load(File.read(File.expand_path( '../../paths.yml',__FILE__)))
+   	   @paths.each do |path|
+		 model = Object.const_set(path['modelname'], Class.new(ActiveRecord::Base) {})
+		 id = model.last.id
+		 url = '/api/v1/'+path['path']+'/'+id.to_s+'.json'
+		 get url
+                 assert last_response.ok?
+	 	 content = JSON.parse(last_response.body)
+		 assert !content.nil?
+                 assert_equal model.last.to_json, content.to_json		 
+	   end 	    
+	end	
 end
